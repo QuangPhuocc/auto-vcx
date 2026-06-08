f21faaa14129a8e1ff4726395629cd2443e65b06
Quang Phuoc <quangphuoc.job@gmail.com>
2026-06-08 14:28:30 +0700

    Fix Vite allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
@@ -16,9 +16,15 @@ export default defineConfig(({mode}) => {
       },
     },
     server: {
+      allowedHosts: [
+        'autovcx-phuoclq.xyz',
+        'admin.autovcx-phuoclq.xyz'
+      ],
+
       watch: {
         ignored: ['**/server/data/**']
       },
+
       proxy: {
         '/api': {
           target: 'http://localhost:5000',
